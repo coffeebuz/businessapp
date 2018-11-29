0be5fdc58434aa438241e248cbe71158849a6206
Kerrianne Sim <kerrianne.sim@xero.com>
2018-11-29 23:17:22 +1100

    set qr scanner in portrait mode

--- Coffeebuzbiz/app/src/main/java/com/example/kerriannesim/coffeebuzbiz/menu/NewCustomerScanner.kt
+++ Coffeebuzbiz/app/src/main/java/com/example/kerriannesim/coffeebuzbiz/menu/NewCustomerScanner.kt
@@ -5,8 +5,11 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.kerriannesim.coffeebuzbiz.CaptureActivityPortrait
 import com.example.kerriannesim.coffeebuzbiz.R
 import com.google.zxing.integration.android.IntentIntegrator
+
+
 
 class NewCustomerFragment : Fragment() {
 
@@ -23,7 +26,8 @@ class NewCustomerFragment : Fragment() {
         integrator.setPrompt("Scan a barcode")
         integrator.setCameraId(0)  // Use a specific camera of the device
         integrator.setBeepEnabled(false)
-        integrator.setOrientationLocked(false)
+        integrator.setOrientationLocked(true)
+        integrator.captureActivity = CaptureActivityPortrait::class.java
         integrator.setBarcodeImageEnabled(true)
         integrator.initiateScan()
         return inflater.inflate(R.layout.new_customer_main, container, false)
